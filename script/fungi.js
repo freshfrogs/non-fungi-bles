a3b58da7b375d4862743dd7ec9e699c54edd1e1f
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2022-12-15 01:53:18 -0600

    Update fungi.js

--- script/fungi.js
+++ script/fungi.js
@@ -60,7 +60,7 @@
         mint_quantity = mint_quantity + 1;
         mint_total = mint_price*mint_quantity;
         document.getElementById('mint_quantity').innerHTML = mint_quantity
-        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(2)+'Ξ'
+        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
         document.getElementById('mintImage').src = '../build/images/'+((next_id+mint_quantity)-1)+'.png'
         document.getElementById('button_middle').innerHTML = '<strong>Mushroom</strong>'+((next_id+mint_quantity)-1)
       }
@@ -73,7 +73,7 @@
         mint_quantity = mint_quantity - 1;
         mint_total = mint_price*mint_quantity;
         document.getElementById('mint_quantity').innerHTML = mint_quantity
-        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(2)+'Ξ'
+        document.getElementById('button_left').innerHTML = '<strong>Mint</strong>'+mint_total.toFixed(3)+'Ξ'
         document.getElementById('mintImage').src = '../build/images/'+((next_id+mint_quantity)-1)+'.png'
         document.getElementById('button_middle').innerHTML = '<strong>Mushroom</strong>'+((next_id+mint_quantity)-1)
       }
